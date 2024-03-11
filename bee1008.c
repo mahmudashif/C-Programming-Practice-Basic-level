0af0c115d8a99460c031a4bdeb3ae5c764c3532b
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-11 17:53:03 +0600

    bee 1008 solved and accepted

---
+++ bee1008.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int num,a;
+    float b;
+    scanf("%d%d%f",&num,&a,&b);
+    printf("NUMBER = %d\n",num);
+    printf("SALARY = U$ %.2lf\n",a*b);
+    return 0;
+}
